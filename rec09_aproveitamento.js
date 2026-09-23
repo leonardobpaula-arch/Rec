2ddba8ff1554abcad93ce35846819a40542c9710
leonardo <leonardo.b.paula@edu.senai.br>
2026-09-23 11:24:18 -0300

    7

--- rec09_aproveitamento.js
+++ rec09_aproveitamento.js
@@ -1,4 +1,6 @@
 const entrada = require('readline-sync');
 
+function aproveitamento {
 const total = entrada.questionInt("Qual o valor total? ");
 const util = entrada.questionInt("Qual o valor util? ");
+}
